2d640e80155e1d94be2b6420d9aee7e19b37edf3
frankv723 <31058518+frankv723@users.noreply.github.com>
2021-03-07 20:02:02 +0800

    Comments and Naming

--- src/maingame/Login.java
+++ src/maingame/Login.java
@@ -24,13 +24,15 @@ import java.awt.event.FocusEvent;
 
 
 public class Login {
+	
+	// Set Global Variables ------------------------------------------
 	Credentials id1;
 	
 	public JFrame frame;
 	private JTextField txtUsername;
-	private JPasswordField passwordField;
+	private JPasswordField txtPassword;
 	
-	// Launch the application
+	// Launch the application-----------------------------------------
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -49,14 +51,20 @@ public class Login {
 	 * Create the application.
 	 */
 	public Login(Credentials id) {
+		
+		// initializes Credentials.java and/or receive its data ----------------------------------
 		id1 = id;
 		initialize();
 	}
 	
 	private boolean checkFields(){
 	
+		/* 
+		 * Checks all Fields if its empty and matches required format ------------------------------------
+		 */
+		
 		String uName = txtUsername.getText();
-		String pass1 = String.valueOf(passwordField.getPassword());
+		String pass1 = String.valueOf(txtPassword.getPassword());
 		String usName = id1.getUname();
 		String opass = id1.getpass1();
 		
@@ -71,20 +79,18 @@ public class Login {
 		}
 		
 	}
-	private void eraseMem(){
-		 id1.setUname(null);
-		 id1.setpass1(null);
-		
-	}
+	
 	
 	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// JFrame location -------------------------------------------------------
 		Dimension ss = Toolkit.getDefaultToolkit().getScreenSize();
 		Dimension frameSize = new Dimension (800, 484);
 		
+		// JFrame initialization -------------------------------------------------
 		frame = new JFrame();
 		frame.setUndecorated(true);
 		frame.getContentPane().setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
@@ -93,14 +99,44 @@ public class Login {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		// Tic Tac Toe Gif -------------------------------------------------------
 		JLabel lblNewLabel_6 = new JLabel("");
 		Image img5 = new ImageIcon(this.getClass().getResource("/TicTac150p.gif")).getImage();
 		lblNewLabel_6.setIcon(new ImageIcon(img5));
 		lblNewLabel_6.setBounds(193, 33, 150, 150);
 		frame.getContentPane().add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_1 = new JLabel("SignUp");
-		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+		//Exit Button---------------------------------------------------------------
+		JButton Close = new JButton("X");
+		Close.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		Close.setFocusPainted(false);
+		Close.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 15));
+		Close.setBackground(new Color(62, 62, 62));
+		Close.setForeground(new Color(255, 255, 255));
+		Close.setBounds(745, 11, 45, 30);
+		frame.getContentPane().add(Close);
+		
+		// Minimize Button -----------------------------------------------------------
+		JButton minimize = new JButton("_");
+		minimize.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setState(JFrame.ICONIFIED);
+			}
+		});
+		minimize.setFocusPainted(false);
+		minimize.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 15));
+		minimize.setBackground(new Color(62, 62, 62));
+		minimize.setForeground(new Color(255, 255, 255));
+		minimize.setBounds(697, 11, 45, 30);
+		frame.getContentPane().add(minimize);
+		
+		// Signup Tab ------------------------------------------------------------------
+		JLabel signuplbl = new JLabel("SignUp");
+		signuplbl.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				SignUp in = new SignUp(id1);
@@ -108,47 +144,20 @@ public class Login {
 				frame.dispose();
 			}
 		});
-		lblNewLabel_1.setForeground(Color.BLACK);
-		lblNewLabel_1.setFont(new Font("Luckiest Guy", Font.PLAIN, 27));
-		lblNewLabel_1.setBounds(534, 56, 170, 57);
-		frame.getContentPane().add(lblNewLabel_1);
+		signuplbl.setForeground(Color.BLACK);
+		signuplbl.setFont(new Font("Luckiest Guy", Font.PLAIN, 27));
+		signuplbl.setBounds(534, 56, 170, 57);
+		frame.getContentPane().add(signuplbl);
 		
-		JButton btnNewButton = new JButton("X");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		btnNewButton.setFocusPainted(false);
-		btnNewButton.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 15));
-		btnNewButton.setBackground(new Color(62, 62, 62));
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBounds(745, 11, 45, 30);
-		frame.getContentPane().add(btnNewButton);
+		// Username Label and TextField ---------------------------------------------
+		JLabel uNamelbl = new JLabel("Username");
+		uNamelbl.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
+		uNamelbl.setVisible(false);
+		uNamelbl.setForeground(new Color(255, 181, 0));
+		uNamelbl.setBounds(396, 137, 114, 28);
+		frame.getContentPane().add(uNamelbl);
 		
-		JButton btnNewButton_1 = new JButton("_");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.setState(JFrame.ICONIFIED);
-			}
-		});
-		btnNewButton_1.setFocusPainted(false);
-		btnNewButton_1.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 15));
-		btnNewButton_1.setBackground(new Color(62, 62, 62));
-		btnNewButton_1.setForeground(new Color(255, 255, 255));
-		btnNewButton_1.setBounds(697, 11, 45, 30);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		
-		JLabel lblNewLabel_3 = new JLabel("Username");
-		lblNewLabel_3.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
-		lblNewLabel_3.setVisible(false);
-		lblNewLabel_3.setForeground(new Color(255, 181, 0));
-		lblNewLabel_3.setBounds(396, 137, 114, 28);
-		frame.getContentPane().add(lblNewLabel_3);
-		
-		
-		txtUsername = new JTextField();
+		txtUsername = new JTextField("Username");
 		txtUsername.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -157,18 +166,17 @@ public class Login {
 				txtUsername.setText("");
 				
 				}
-				lblNewLabel_3.setVisible(true);
+				uNamelbl.setVisible(true);
 			}
 			@Override
 			public void focusLost(FocusEvent e) {
 				if (txtUsername.getText().trim().equals("") || 
 						txtUsername.getText().trim().toLowerCase().equals("username")) {
 				txtUsername.setText("Username");
-				lblNewLabel_3.setVisible(false);
+				uNamelbl.setVisible(false);
 				}
 			}
 		});
-		txtUsername.setText("Username");
 		txtUsername.setBorder(null);
 		txtUsername.setFont(new Font("Luckiest Guy", Font.PLAIN, 22));
 		txtUsername.setBackground(Color.WHITE);
@@ -176,34 +184,31 @@ public class Login {
 		frame.getContentPane().add(txtUsername);
 		txtUsername.setColumns(10);
 		
-		JLabel lblNewLabel_5 = new JLabel("Login");
-		lblNewLabel_5.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
-		lblNewLabel_5.setForeground(new Color(255, 181, 0));
-		lblNewLabel_5.addMouseListener(new MouseAdapter() {
+		// Login Label/ Button -----------------------------------------------
+		JLabel Loginlbl = new JLabel("Login");
+		Loginlbl.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
+		Loginlbl.setForeground(new Color(255, 181, 0));
+		Loginlbl.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Image img4 = new ImageIcon(this.getClass().getResource("/Ellipse 1.png")).getImage();
-				lblNewLabel_5.setIcon(new ImageIcon(img4));
 				if(checkFields()) {
-					eraseMem();
 					Game tictac = new Game();
 					tictac.frame_1.setVisible(true);
 					frame.dispose();
 				}
 			}
 		});
-		lblNewLabel_5.setBounds(665, 377, 105, 48);
-		frame.getContentPane().add(lblNewLabel_5);
+		Loginlbl.setBounds(665, 377, 105, 48);
+		frame.getContentPane().add(Loginlbl);
 		
-		JLabel lblNewLabel_4 = new JLabel("X");
-		lblNewLabel_4.setFont(new Font("Luckiest Guy", Font.BOLD, 69));
-		lblNewLabel_4.setForeground(new Color(255, 181, 0));
-		lblNewLabel_4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblNewLabel_4.addMouseListener(new MouseAdapter() {
+		JLabel xbutton = new JLabel("X");
+		xbutton.setFont(new Font("Luckiest Guy", Font.BOLD, 69));
+		xbutton.setForeground(new Color(255, 181, 0));
+		xbutton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		xbutton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if(checkFields()) {
-					eraseMem();
 					Game tictac = new Game();
 					tictac.frame_1.setVisible(true);
 					frame.dispose();
@@ -211,53 +216,55 @@ public class Login {
 			}
 		});
 		
-		lblNewLabel_4.setBounds(669, 329, 131, 78);
-		frame.getContentPane().add(lblNewLabel_4); 
+		xbutton.setBounds(669, 329, 131, 78);
+		frame.getContentPane().add(xbutton); 
 		
-		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
-		lblNewLabel_2.setForeground(new Color(255, 181, 0));
-		lblNewLabel_2.setVisible(false);
-		lblNewLabel_2.setBounds(394, 219, 140, 45);
-		frame.getContentPane().add(lblNewLabel_2);
 		
-		passwordField = new JPasswordField("Password");
-		passwordField.setEchoChar((char)0);
-		passwordField.addFocusListener(new FocusAdapter() {
+		// Password Label and Text Field ----------------------------------------------
+		JLabel passwordlbl = new JLabel("Password");
+		passwordlbl.setFont(new Font("Luckiest Guy", Font.PLAIN, 19));
+		passwordlbl.setForeground(new Color(255, 181, 0));
+		passwordlbl.setVisible(false);
+		passwordlbl.setBounds(394, 219, 140, 45);
+		frame.getContentPane().add(passwordlbl);
+		
+		txtPassword = new JPasswordField("Password");
+		txtPassword.setEchoChar((char)0);
+		txtPassword.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
-				if (String.valueOf(passwordField.getPassword()).trim().equals("") || 
-						String.valueOf(passwordField.getPassword()).trim().toLowerCase().equals("password")) {
-					passwordField.setText("");
+				if (String.valueOf(txtPassword.getPassword()).trim().equals("") || 
+						String.valueOf(txtPassword.getPassword()).trim().toLowerCase().equals("password")) {
+					txtPassword.setText("");
 				}
-				lblNewLabel_2.setVisible(true);
-				passwordField.setEchoChar((char)0);
+				passwordlbl.setVisible(true);
+				txtPassword.setEchoChar((char)0);
 				
 			}
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (String.valueOf(passwordField.getPassword()).trim().equals("") || 
-						String.valueOf(passwordField.getPassword()).trim().toLowerCase().equals("password")) {
-					passwordField.setText("Password");
-				lblNewLabel_2.setVisible(false);
-				passwordField.setEchoChar((char)0); 
-				}else if(!String.valueOf(passwordField.getPassword()).trim().equals("") || 
-						!String.valueOf(passwordField.getPassword()).trim().toLowerCase().equals("password")){
-					passwordField.setEchoChar('*'); 
+				if (String.valueOf(txtPassword.getPassword()).trim().equals("") || 
+						String.valueOf(txtPassword.getPassword()).trim().toLowerCase().equals("password")) {
+					txtPassword.setText("Password");
+				passwordlbl.setVisible(false);
+				txtPassword.setEchoChar((char)0); 
+				}else if(!String.valueOf(txtPassword.getPassword()).trim().equals("") || 
+						!String.valueOf(txtPassword.getPassword()).trim().toLowerCase().equals("password")){
+					txtPassword.setEchoChar('*'); 
 				}
 			}
 		});
-		passwordField.setBorder(null);
-		passwordField.setFont(new Font("Luckiest Guy", Font.PLAIN, 22));
-		passwordField.setBounds(405, 260, 322, 45);
-		frame.getContentPane().add(passwordField);
+		txtPassword.setBorder(null);
+		txtPassword.setFont(new Font("Luckiest Guy", Font.PLAIN, 22));
+		txtPassword.setBounds(405, 260, 322, 45);
+		frame.getContentPane().add(txtPassword);
 		
-		
-		JLabel lblNewLabel = new JLabel("");
+		// Background Image --------------------------------------------------
+		JLabel background = new JLabel("");
 		Image img = new ImageIcon(this.getClass().getResource("/LoginSmall.png")).getImage();
-		lblNewLabel.setIcon(new ImageIcon(img));
-		lblNewLabel.setBounds(0, 0, 800, 484);
-		frame.getContentPane().add(lblNewLabel);
+		background.setIcon(new ImageIcon(img));
+		background.setBounds(0, 0, 800, 484);
+		frame.getContentPane().add(background);
 		
 		
 		
